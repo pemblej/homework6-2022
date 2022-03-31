bca0d4cc0aa10164f9f5bc3198080d35ea4c688f
pemblej <97365479+pemblej@users.noreply.github.com>
2022-03-31 11:48:02 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,72 @@
+var video = document.querySelector("#player1")
+
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+    video.autoplay = false
+	video.loop = false	
+
+	let volume = document.querySelector("video").volume
+	document.querySelector("#volume").innerHTML = volume
+})
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video")
+	video.pause()
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate -= 0.05
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate += 0.05
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	var skipTime = video.currentTime + 15
+	
+	if (skipTime > video.duration) {
+		video.currentTime = 0
+		console.log(video.currentTime)
+	}
+
+	else {
+		video.currentTime = skipTime
+		console.log(video.currentTime)
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+		video.muted = true
+		document.querySelector("#mute").innerHTML = "Unmute"
+	}
+
+	else if (video.muted == true) {
+		video.muted = false
+		document.querySelector("#mute").innerHTML = "Mute"
+	}
+});
+
+let volume = document.querySelector("#slider")
+
+volume.addEventListener("change", function(e) {
+	video.volume = e.currentTarget.value / 100;
+	document.querySelector("#volume").innerHTML = video.volume
+})
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	document.querySelector("#player1").classList.add("oldSchool") 
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	document.querySelector("#player1").classList.remove("oldSchool") 
+});
